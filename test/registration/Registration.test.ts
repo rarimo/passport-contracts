ba696d8e569ecd0c97e0d009defcc8cdeb02a316
Artem Chystiakov <artem.ch31@gmail.com>
2024-04-03 13:57:26 +0300

    as promised

--- test/registration/Registration.test.ts
+++ test/registration/Registration.test.ts
@@ -66,7 +66,7 @@ describe("Registration", () => {
       await registration.register(poseidon2PubKey, signature, modulus, formattedProof, someHash);
     });
 
-    it.skip("should register", async () => {
+    it("should register", async () => {
       const icaoMerkleRoot = "0x2c50ce3aa92bc3dd0351a89970b02630415547ea83c487befbc8b1795ea90c45";
       const poseidon2PubKey = "0xced3831dcf3a96049d93cbbd3eaa0010f3b97a70ef591269c8975afb4717e5";
       const group1Hash = "0x206187ab789aca5d2073582ba86f76c57f741ed2c78ccb96d79a9600b49df8d6";
@@ -79,13 +79,24 @@ describe("Registration", () => {
         "0xae782184c70d1c9829be95f23b2c21abf5a82019a6648b933ca8abe4dc837582068d45d0b5f94cc4cd4c7cde9bef0f4d79534469997d95018e6391d294000d536c2654f79a829ff8cb74a32fdbbab73e16cab87ff600344ef9dda6cc11c4d67672d66e875bbacd4de1e5b2d4efdd50b027bc16f357218c345861c1bc8f38b28d";
 
       const formattedProof: VerifierHelper.ProofPointsStruct = {
-        a: ["", ""],
-
-        b: [
-          ["", ""],
-          ["", ""],
+        a: [
+          "1622041246541719364375676140649029264387193270370653169743832721239417243214",
+          "722625069376678952553619045676340161711899918862386545751619200743655527397",
         ],
-        c: ["", ""],
+        b: [
+          [
+            "6572050127215579342758288551258667828591362018934803711682159999142616045106",
+            "1996793900476578136991719456987382418754060631791252473062149656295472929065",
+          ],
+          [
+            "11474703322214281363488857985126449840456992969047601331951461879251837090845",
+            "9914353967148169935499110567230984688508910023569876879292780630014387079953",
+          ],
+        ],
+        c: [
+          "7623491183446532677235094307063287140332555288468746029766903390272933720592",
+          "1219548073783994741160491826962226753539985534696345160639380550580308251209",
+        ],
       };
 
       await registration.register(poseidon2PubKey, signature, modulus, formattedProof, group1Hash);
