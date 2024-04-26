b003b5706f790c82ca2f845ecdc867b550fa64c8
Artem Chystiakov <artem.ch31@gmail.com>
2024-04-26 20:27:55 +0300

    removed unused function

--- test/helpers/TSSSigner.ts
+++ test/helpers/TSSSigner.ts
@@ -4,15 +4,6 @@ import { BigNumberish, BytesLike, HDNodeWallet } from "ethers";
 export class TSSSigner {
   constructor(public signer: HDNodeWallet) {}
 
-  public changeICAOMasterTreeRoot(newRoot: string, timestamp: BigNumberish): string {
-    const hash = ethers.solidityPackedKeccak256(
-      ["string", "bytes32", "uint64"],
-      ["Rarimo CSCA root", newRoot, timestamp],
-    );
-
-    return this.sign(hash);
-  }
-
   public signChangeSigner(newPubKey: string): string {
     const hash = ethers.solidityPackedKeccak256(["bytes"], [newPubKey]);
 
