9fe29ce76bfdfe0d6d6c9c6e8c21d506e50afc57
Artem Chystiakov <artem.ch31@gmail.com>
2024-05-07 16:31:35 +0300

    removed unused dispatcher type

--- contracts/registration/PassportTypes.sol
+++ contracts/registration/PassportTypes.sol
@@ -2,5 +2,4 @@
 pragma solidity 0.8.16;
 
 bytes32 constant RSA_SHA1_2688 = keccak256("RSA_SHA1_2688");
-bytes32 constant RSA_SHA1_2688_TIMESTAMP = keccak256("RSA_SHA1_2688_TIMESTAMP");
 bytes32 constant ECDSA_SHA1_2704 = keccak256("ECDSA_SHA1_2704");
--- deploy/2_dispatchers.migration.ts
+++ deploy/2_dispatchers.migration.ts
@@ -9,7 +9,7 @@ import {
   RSAECDSAVerifier__factory,
 } from "@ethers-v6";
 
-import { ECDSA_SHA1_2704, RSA_SHA1_2688, RSA_SHA1_2688_TIMESTAMP } from "@/scripts/utils/passport-types";
+import { ECDSA_SHA1_2704, RSA_SHA1_2688 } from "@/scripts/utils/passport-types";
 
 const deployVerifiers = async (deployer: Deployer) => {
   await deployer.deploy(RSAECDSAVerifier__factory);
@@ -17,20 +17,8 @@ const deployVerifiers = async (deployer: Deployer) => {
 
 const deployRSASHA12688Dispatcher = async (deployer: Deployer) => {
   const verifier = await deployer.deployed(RSAECDSAVerifier__factory);
-  const authenticator = await deployer.deploy(RSASHA1Authenticator__factory, { name: "RSASHA12688Authenticator" });
-  const dispatcher = await deployer.deploy(RSASHA1Dispatcher__factory, { name: "RSASHA12688Dispatcher" });
-
-  await dispatcher.__RSASHA1Dispatcher_init(await authenticator.getAddress(), await verifier.getAddress());
-
-  return dispatcher;
-};
-
-const deployRSASHA12688TimestampDispatcher = async (deployer: Deployer) => {
-  const verifier = await deployer.deployed(RSAECDSAVerifier__factory);
-  const authenticator = await deployer.deploy(RSASHA1Authenticator__factory, {
-    name: "RSASHA12688TimestampAuthenticator",
-  });
-  const dispatcher = await deployer.deploy(RSASHA1Dispatcher__factory, { name: "RSASHA12688TimestampDispatcher" });
+  const authenticator = await deployer.deploy(RSASHA1Authenticator__factory);
+  const dispatcher = await deployer.deploy(RSASHA1Dispatcher__factory);
 
   await dispatcher.__RSASHA1Dispatcher_init(await authenticator.getAddress(), await verifier.getAddress());
 
@@ -53,16 +41,13 @@ export = async (deployer: Deployer) => {
   const registration = await deployer.deployed(Registration__factory);
 
   const rsaSha12688Dispatcher = await deployRSASHA12688Dispatcher(deployer);
-  const rsaSha12688TimestampDispatcher = await deployRSASHA12688TimestampDispatcher(deployer);
   const ecdsaSha12704Dispatcher = await deployECDSASHA12704Dispatcher(deployer);
 
   await registration.addDispatcher(RSA_SHA1_2688, await rsaSha12688Dispatcher.getAddress());
-  await registration.addDispatcher(RSA_SHA1_2688_TIMESTAMP, await rsaSha12688TimestampDispatcher.getAddress());
   await registration.addDispatcher(ECDSA_SHA1_2704, await ecdsaSha12704Dispatcher.getAddress());
 
   Reporter.reportContracts(
     ["RSASHA12688Dispatcher", `${await rsaSha12688Dispatcher.getAddress()}`],
-    ["RSASHA12688TimestampDispatcher", `${await rsaSha12688TimestampDispatcher.getAddress()}`],
     ["ECDSASHA12704Dispatcher", `${await ecdsaSha12704Dispatcher.getAddress()}`],
   );
 };
