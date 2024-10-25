e2ddb1630c1b0b9a75346cde7121d0d95579438c
dovgopoly <idovgopoly@gmail.com>
2024-10-25 18:42:29 +0300

    cleaned up cfg

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -90,28 +90,14 @@ const config: HardhatUserConfig = {
     },
   },
   solidity: {
-    compilers: [
-      {
-        version: "0.8.16",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-          viaIR: false,
-        },
+    version: "0.8.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
-      {
-        version: "0.8.17",
-        settings: {
-          optimizer: {
-            enabled: false,
-            runs: 200,
-          },
-          viaIR: false,
-        },
-      },
-    ],
+      viaIR: false,
+    },
   },
   etherscan: {
     apiKey: {
