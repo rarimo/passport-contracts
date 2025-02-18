5105a054eaca03642ce0fdb3db52f862daa30dbc
Kyryl R <kyryl.ryabov@gmail.com>
2025-02-18 14:10:44 +0200

    Set address of the evidenceRegistry

--- deploy/config/localhost.ts
+++ deploy/config/localhost.ts
@@ -1,5 +1,5 @@
 export const treeSize = 80;
-export const evidenceRegistry = "0x0000000000000000000000000000000000000000";
+export const evidenceRegistry = "0x781268D46a654D020922f115D75dd3D56D287812";
 export const owner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 export const icaoMasterTreeMerkleRoot = "0xca09a639ceafe2c7b3d37f1ddd78ae0b203332a3e7b180aa35435a0d3a8cd8c7";
 export const registrationName = "SecondRegistration";
