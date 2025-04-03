9cb958e33899bc44bd1fe9f8ebbb5eec66f205e5
Kyryl R <kyryl.ryabov@gmail.com>
2025-04-03 11:42:41 +0300

    Added Rarimo Beta network. Fixed type errors

---
+++ deploy/config/rarimo-beta.ts
@@ -0,0 +1,13 @@
+export const treeSize = 80;
+export const owner = "0xD2D15f650654d69F4dFd3aF41c49412c62BD616F";
+
+export const evidenceRegistry = "0x156C5fb747638F18264080e1f17A15B76E938e73";
+
+export const icaoMasterTreeMerkleRoot = "0x0eef24f25513eb3d3e13856805319553013158c347209287d090d3100e33c55b";
+
+export const registrationName = "SecondRegistration";
+export const simpleRegistrationName = "SimpleRegistration";
+export const simpleRegistrationSigners = ["0xD2D15f650654d69F4dFd3aF41c49412c62BD616F"];
+
+export const registration2 = "0x0000000000000000000000000000000000000000";
+export const stateKeeper = "0x0000000000000000000000000000000000000000";
